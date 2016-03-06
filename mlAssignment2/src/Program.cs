ae560cfff0a0423f81582ba741824c8529b1d484
Mark Lohstroh <mlohstroh@gmail.com>
2016-03-06 14:00:35 -0600

    adding argument parsing for the assignment

--- mlAssignment2/src/Program.cs
+++ mlAssignment2/src/Program.cs
@@ -11,21 +11,60 @@ namespace mlAssignment2
     {
         public static void Main(string[] args)
         {
-            List<DataRow> train = DataRow.ReadFile("data\\train2-win.dat");
-            List<DataRow> test = DataRow.ReadFile("data\\test2-win.dat");
+            string trainFile = "data\\train2-win.dat";
+            string testFile = "data\\test2-win.dat";
+            float learningRate = 0.3f;
+            int numIterations = 400;
+
+            if (args.Length >= 4)
+            {
+                // It goes 
+                // trainFile testFile learningRate and numberofIterations
+                
+                trainFile = args[0];
+                testFile = args[1];
+
+                try
+                {
+                    learningRate = float.Parse(args[2]);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Unable to parse learning rate. Correct example: 0.4");
+                }
+
+                try
+                {
+                    numIterations = int.Parse(args[3]);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Unable to parse number of iterations. Correct example: 400");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You provided an incorrect number of inputs... Defauting to the packaged parameters");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+
+            List<DataRow> train = DataRow.ReadFile(trainFile);
+            List<DataRow> test = DataRow.ReadFile(testFile);
 
             Perceptron p = new Perceptron(train)
             {
                 DefaultEdgeWeight = 0,
-                LearningRate = 0.9f,
+                LearningRate = learningRate,
                 Threshold = 0.5f,
-                MaxIterations = 800
+                MaxIterations = numIterations
             };
 
             p.Train(train);
 
             float trainAccuracy = p.Classify(train);
             float testAccuracy = p.Classify(test);
+            Console.WriteLine();
             Console.WriteLine("Accuracy on training set ({0} instances): {1}%", train.Count, trainAccuracy);
             Console.WriteLine("Accuracy on test set ({0} instances): {1}%", test.Count, testAccuracy);
 
